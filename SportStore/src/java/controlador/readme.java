f157deb199fbfbc20387a720e19987b1e808667a
mhurtado-2021407 <mhurtado-2021407@kinal.edu.gt>
2022-08-31 20:59:17 -0600

    se agregaron los paquetes de modelo y controlador

---
+++ SportStore/src/java/controlador/readme.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author mario
+ */
+public class readme {
+    
+    // este java class es por si se elimina los paquetes, si no estan eliminados por favor no tocar :3 
+    // gracias wapos 
+}
